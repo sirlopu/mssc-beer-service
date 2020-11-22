3ab06306388244a3f2723290eec91af18358659b
Gabriel S <sirlopug@gmail.com>
2020-11-21 22:38:45 -0800

    Added beer repository. Closes #6

---
+++ src/main/java/com/sirlopu/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.sirlopu.msscbeerservice.repositories;
+
+import com.sirlopu.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
