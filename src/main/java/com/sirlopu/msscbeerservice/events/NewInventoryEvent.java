6e18b38d308fb94d2eb2db4dfdc7d4eb27fc7086
Gabriel S <sirlopug@gmail.com>
2021-02-15 19:33:16 -0800

    Created beer event objects - Close #3

---
+++ src/main/java/com/sirlopu/msscbeerservice/events/NewInventoryEvent.java
@@ -0,0 +1,9 @@
+package com.sirlopu.msscbeerservice.events;
+
+import com.sirlopu.msscbeerservice.web.model.BeerDto;
+
+public class NewInventoryEvent extends BeerEvent {
+    public NewInventoryEvent(BeerDto beerDto) {
+        super(beerDto);
+    }
+}
