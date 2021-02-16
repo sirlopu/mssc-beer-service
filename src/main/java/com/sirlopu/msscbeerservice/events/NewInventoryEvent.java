4621cb8b3f3ad5ddb7e1075d3414b7dc3253a457
Gabriel S <sirlopug@gmail.com>
2021-02-15 19:34:27 -0800

    Revert "Created beer event objects - Close #25"
    This reverts commit 6e18b38d

--- src/main/java/com/sirlopu/msscbeerservice/events/NewInventoryEvent.java
+++
@@ -1,9 +0,0 @@
-package com.sirlopu.msscbeerservice.events;
-
-import com.sirlopu.msscbeerservice.web.model.BeerDto;
-
-public class NewInventoryEvent extends BeerEvent {
-    public NewInventoryEvent(BeerDto beerDto) {
-        super(beerDto);
-    }
-}
